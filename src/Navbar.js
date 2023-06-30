044013651d5b8f526ddec2ad12bc9f95aabb0946
amitsevda2303 <137678838+amitsevda2303@users.noreply.github.com>
2023-06-30 12:04:17 +0530

    02

--- src/Navbar.js
+++ src/Navbar.js
@@ -9,7 +9,7 @@ export default function Amit(props) {
       <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode} `}>
 
         <div className="container-fluid Amit">
-          <Link className="navbar-brand" to="/">Navbar</Link>
+          {/* <Link className="navbar-brand" to="/">Navbar</Link> */}
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
